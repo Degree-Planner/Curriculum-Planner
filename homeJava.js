4dfb63eccd7a568cca07175ecc369a8197b71652
jpaluso <69777469+jpaluso@users.noreply.github.com>
2021-03-04 21:17:08 -0600

    Add files via upload

---
+++ homeJava.js
@@ -0,0 +1,13 @@
+function openAllDegreesPage(){
+	window.location.replace("Paluso_ch4.html")
+}
+function openAdminLoginPage(){
+	window.location.replace("https://www.google.com")
+}
+function redirect() {
+	window.location = "http://www.google.com/"
+
+}
+function myFunction(link) {
+  location.replace("pages/Paluso_ch4.html")
+}
