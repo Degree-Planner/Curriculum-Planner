639f08a03903cb591a995565a4e85feb1926a08c
nrudolph1 <47006419+nrudolph1@users.noreply.github.com>
2021-03-14 15:10:53 -0500

    Delete CSC 530 Curriculum-Planner directory

--- CSC 530 Curriculum-Planner/client/src/api/index.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const API = axios.create({ baseURL: 'http://localhost:5000'});
-
-export const fetchCourses = () => API.get('/courses');
-
-export const createCourse = (newCourse) => API.post('/courses', newCourse);
-
-export const authAdmin = (email) => API.post('/admins', email);
