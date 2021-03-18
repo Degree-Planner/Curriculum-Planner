a220ef061a876d699374c4d4f4fcd02f3822da45
Jonathan-Crafton <69919179+Jonathan-Crafton@users.noreply.github.com>
2021-03-18 15:53:15 -0500

    Add files via upload

--- client/src/actions/degrees.js
+++ client/src/actions/degrees.js
@@ -9,3 +9,25 @@ export const getDegrees = () => async (dispatch) => {
         console.log(error.message);
     }
 }
+export const createDegree = (degree) => async (dispatch) => {
+    try{
+        const {data} = await api.createDegree(degree);
+
+        dispatch({type: 'CREATE', payload: data});
+        
+    }
+    catch(error){
+        console.log(error.message);
+    }
+}
+
+export const updateDegree = (degree) => async (dispatch) => {
+    try{
+          const {data} = await api.updateDegree(degree);
+          
+          dispatch({type: 'UPDATE', payload: data});
+    }
+    catch(error){
+        console.log(error.message);
+    }
+}
