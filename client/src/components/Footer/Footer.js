1da2c737d0f391e6ee5a830dd848f95d85064d16
jpaluso <69777469+jpaluso@users.noreply.github.com>
2021-03-18 20:31:43 -0500

    Add files via upload

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from 'react';
+import { AppBar, Typography, Grid, Divider } from '@material-ui/core';
+import { Link, useHistory, useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+import useStyles from './styles';
+
+const Footer = () => {
+    const classes = useStyles();    
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const location = useLocation();
+
+    const logout = () => {
+        dispatch({ type: 'LOGOUT' });
+
+        history.push('/csc530/dev');
+
+        setUser(null);
+    };
+
+    useEffect(() => {
+        //const token = user?.token;
+
+        setUser(JSON.parse(localStorage.getItem('profile')))
+    }, [location]);
+
+    return (
+    <AppBar className={classes.footer} position="fixed" color="inherit">
+        <Grid container spacing={0}>
+            <Grid item xs={6}>
+                <Typography className={classes.heading} variant="h5" align="center">Links</Typography>
+            </Grid>
+            <Grid item xs={6}>
+                <Typography className={classes.heading} variant="h5" align="center">Contributors</Typography>
+            </Grid>
+            <Grid item xs={6}>
+                <center>
+                    <a className={classes.a} href="https://www.murraystate.edu/academics/CollegesDepartments/CollegeOfBusiness/index.aspx">College Of Business</a>
+                </center>
+            </Grid>
+            <Grid item xs={6}>
+                <center>
+                    <Typography className={classes.heading} variant="p" align="center">Jonathan Crafton</Typography>
+                </center>
+            </Grid>
+            <Grid item xs={6}>
+                <center>
+                    <a className={classes.a} href="http://campus.murraystate.edu/classes/">RacerNet</a>
+                </center>
+            </Grid>
+            <Grid item xs={6}>
+                <center>
+                    <Typography className={classes.heading} variant="p" align="center">Jacob Paluso</Typography>
+                </center>
+            </Grid>
+            <Grid item xs={6}>
+                <center>
+                    <a className={classes.a} href="https://www.murraystate.edu/">Murray State Main Webpage</a>
+                </center>
+            </Grid>
+            <Grid item xs={6}>
+                <center>
+                    <Typography className={classes.heading} variant="p" align="center">Noah Rudolph</Typography>
+                </center>
+            </Grid>
+            <br></br>
+            <br></br>
+            <Grid item xs={12}>
+                <Divider className={classes.divider} variant="middle" />
+            </Grid>
+            <br></br>
+            <Grid item xs={12}>
+                <center>
+                    <Typography className={classes.heading} variant="p" align="center">&copy;{new Date().getFullYear()} CSC 530 Degree Plan Project | All rights reserved </Typography>
+                </center>
+            </Grid>
+      </Grid>
+    </AppBar>
+    )};
+
+export default Footer;
---
+++ client/src/components/Footer/styles.js
@@ -0,0 +1,20 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  footer:{
+    top: 'auto',
+    bottom: 0,
+    background: '#000080',
+    padding: '10px 50px',
+  },
+  heading: {
+    color: 'rgba(255,255,255, 1)',
+    textDecoration: 'none',
+  },
+  a: {
+    color: 'white'
+  },
+  divider: {
+      background: 'white'
+  }
+}));
