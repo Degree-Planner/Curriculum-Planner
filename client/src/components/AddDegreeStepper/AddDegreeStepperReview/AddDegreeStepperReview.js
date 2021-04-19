255b2dd96992099d356a80fc24afb2454372c90d
Jacob Paluso <jpaluso@murraystate.edu>
2021-04-18 19:18:53 -0500

    All 8 Terms are present at the end of the Add Degree Stepper now

--- client/src/components/AddDegreeStepper/AddDegreeStepperReview/AddDegreeStepperReview.js
+++ client/src/components/AddDegreeStepper/AddDegreeStepperReview/AddDegreeStepperReview.js
@@ -22,7 +22,7 @@ const AddDegreeStepperReview = ({degreeInformation, courseInformation}) => {
         for(var i=0; i<courseInformation.length; i++){
             console.log("Iterating 1");
             console.log(courseInformation[i].Term);
-            if (courseInformation[i].Term == 1 || courseInformation[i].Term == "1" || courseInformation[i].Term == '1'){
+            if (courseInformation[i].Term == 1){
                 console.log("Found Term 1");
                 temp.push(courseInformation[i]);
             }
@@ -35,7 +35,7 @@ const AddDegreeStepperReview = ({degreeInformation, courseInformation}) => {
         for(var i=0; i<courseInformation.length; i++){
             console.log("Iterating 2");
             console.log(courseInformation[i].Term);
-            if (courseInformation[i].Term == 2 || courseInformation[i].Term == "2" || courseInformation[i].Term == '2'){
+            if (courseInformation[i].Term == 2){
                 console.log("Found Term 2");
                 temp.push(courseInformation[i]);
             }
@@ -48,7 +48,7 @@ const AddDegreeStepperReview = ({degreeInformation, courseInformation}) => {
         for(var i=0; i<courseInformation.length; i++){
             console.log("Iterating 3");
             console.log(courseInformation[i].Term);
-            if (courseInformation[i].Term == 3 || courseInformation[i].Term == "3" || courseInformation[i].Term == '3'){
+            if (courseInformation[i].Term == 3){
                 console.log("Found Term 3");
                 temp.push(courseInformation[i]);
             }
@@ -61,8 +61,60 @@ const AddDegreeStepperReview = ({degreeInformation, courseInformation}) => {
         for(var i=0; i<courseInformation.length; i++){
             console.log("Iterating 4");
             console.log(courseInformation[i].Term);
-            if (courseInformation[i].Term == 4 || courseInformation[i].Term == "4" || courseInformation[i].Term == '4'){
+            if (courseInformation[i].Term == 4){
                 console.log("Found Term 4");
+                temp.push(courseInformation[i]);
+            }
+        }
+        return temp;
+    }
+    const term5Courses = () =>{
+        console.log("Scanning Term5");
+        var temp = [];
+        for(var i=0; i<courseInformation.length; i++){
+            console.log("Iterating 5");
+            console.log(courseInformation[i].Term);
+            if (courseInformation[i].Term == 5){
+                console.log("Found Term 5");
+                temp.push(courseInformation[i]);
+            }
+        }
+        return temp;
+    }
+    const term6Courses = () =>{
+        console.log("Scanning Term6");
+        var temp = [];
+        for(var i=0; i<courseInformation.length; i++){
+            console.log("Iterating 6");
+            console.log(courseInformation[i].Term);
+            if (courseInformation[i].Term == 6){
+                console.log("Found Term 6");
+                temp.push(courseInformation[i]);
+            }
+        }
+        return temp;
+    }
+    const term7Courses = () =>{
+        console.log("Scanning Term7");
+        var temp = [];
+        for(var i=0; i<courseInformation.length; i++){
+            console.log("Iterating 7");
+            console.log(courseInformation[i].Term);
+            if (courseInformation[i].Term == 7){
+                console.log("Found Term 7");
+                temp.push(courseInformation[i]);
+            }
+        }
+        return temp;
+    }
+    const term8Courses = () =>{
+        console.log("Scanning Term8");
+        var temp = [];
+        for(var i=0; i<courseInformation.length; i++){
+            console.log("Iterating 8");
+            console.log(courseInformation[i].Term);
+            if (courseInformation[i].Term == 8){
+                console.log("Found Term 8");
                 temp.push(courseInformation[i]);
             }
         }
@@ -199,6 +251,158 @@ const AddDegreeStepperReview = ({degreeInformation, courseInformation}) => {
 
             <Typography className={classes.subheading} variant="h6" align="left">Term 4: </Typography>
             {term4Courses().map((courseInformation) => (
+            <Container maxWidth="sm" className={classes.container}>
+              <Accordion className={classes.card}>
+                  <AccordionSummary 
+                      expandIcon={<ExpandMoreIcon />}>
+                      <div>
+                          <Typography className={classes.title} variant="body1">{courseInformation.CourseID} {courseInformation.CourseTitle}</Typography>
+                      </div>
+                  </AccordionSummary>
+                  <AccordionDetails className={classes.details}>
+                      <div>
+                          <Typography className={classes.details} variant="body4">{courseInformation.CourseDescription}</Typography>
+                      </div>
+                  </AccordionDetails>
+                  <AccordionDetails className={classes.details}>
+                      <div>
+                          <Typography className={classes.details} variant="body5">Minimum Grade: {courseInformation.MinimumGrade.toUpperCase()}</Typography>
+                      </div>
+                  </AccordionDetails>
+                  <AccordionDetails className={classes.details}>
+                      <div>
+                          <Typography className={classes.details} variant="body6">Credit Hours: {courseInformation.CreditHours}</Typography>
+                      </div>
+                  </AccordionDetails>
+                  <AccordionDetails className={classes.details}>
+                      <div>
+                          <Typography className={classes.details} variant="body7">Prerequisites: {courseInformation.PreReqs}</Typography>
+                      </div>
+                  </AccordionDetails>
+                  <AccordionDetails className={classes.details}>
+                      <div>
+                          <Typography className={classes.details} variant="body8">Corequisites: {courseInformation.CoReqs}</Typography>
+                      </div>
+                  </AccordionDetails>
+              </Accordion>
+            </Container>
+            ))}
+                        <Typography className={classes.subheading} variant="h6" align="left">Term 5: </Typography>
+            {term5Courses().map((courseInformation) => (
+            <Container maxWidth="sm" className={classes.container}>
+              <Accordion className={classes.card}>
+                  <AccordionSummary 
+                      expandIcon={<ExpandMoreIcon />}>
+                      <div>
+                          <Typography className={classes.title} variant="body1">{courseInformation.CourseID} {courseInformation.CourseTitle}</Typography>
+                      </div>
+                  </AccordionSummary>
+                  <AccordionDetails className={classes.details}>
+                      <div>
+                          <Typography className={classes.details} variant="body4">{courseInformation.CourseDescription}</Typography>
+                      </div>
+                  </AccordionDetails>
+                  <AccordionDetails className={classes.details}>
+                      <div>
+                          <Typography className={classes.details} variant="body5">Minimum Grade: {courseInformation.MinimumGrade.toUpperCase()}</Typography>
+                      </div>
+                  </AccordionDetails>
+                  <AccordionDetails className={classes.details}>
+                      <div>
+                          <Typography className={classes.details} variant="body6">Credit Hours: {courseInformation.CreditHours}</Typography>
+                      </div>
+                  </AccordionDetails>
+                  <AccordionDetails className={classes.details}>
+                      <div>
+                          <Typography className={classes.details} variant="body7">Prerequisites: {courseInformation.PreReqs}</Typography>
+                      </div>
+                  </AccordionDetails>
+                  <AccordionDetails className={classes.details}>
+                      <div>
+                          <Typography className={classes.details} variant="body8">Corequisites: {courseInformation.CoReqs}</Typography>
+                      </div>
+                  </AccordionDetails>
+              </Accordion>
+            </Container>
+            ))}
+                        <Typography className={classes.subheading} variant="h6" align="left">Term 6: </Typography>
+            {term6Courses().map((courseInformation) => (
+            <Container maxWidth="sm" className={classes.container}>
+              <Accordion className={classes.card}>
+                  <AccordionSummary 
+                      expandIcon={<ExpandMoreIcon />}>
+                      <div>
+                          <Typography className={classes.title} variant="body1">{courseInformation.CourseID} {courseInformation.CourseTitle}</Typography>
+                      </div>
+                  </AccordionSummary>
+                  <AccordionDetails className={classes.details}>
+                      <div>
+                          <Typography className={classes.details} variant="body4">{courseInformation.CourseDescription}</Typography>
+                      </div>
+                  </AccordionDetails>
+                  <AccordionDetails className={classes.details}>
+                      <div>
+                          <Typography className={classes.details} variant="body5">Minimum Grade: {courseInformation.MinimumGrade.toUpperCase()}</Typography>
+                      </div>
+                  </AccordionDetails>
+                  <AccordionDetails className={classes.details}>
+                      <div>
+                          <Typography className={classes.details} variant="body6">Credit Hours: {courseInformation.CreditHours}</Typography>
+                      </div>
+                  </AccordionDetails>
+                  <AccordionDetails className={classes.details}>
+                      <div>
+                          <Typography className={classes.details} variant="body7">Prerequisites: {courseInformation.PreReqs}</Typography>
+                      </div>
+                  </AccordionDetails>
+                  <AccordionDetails className={classes.details}>
+                      <div>
+                          <Typography className={classes.details} variant="body8">Corequisites: {courseInformation.CoReqs}</Typography>
+                      </div>
+                  </AccordionDetails>
+              </Accordion>
+            </Container>
+            ))}
+                        <Typography className={classes.subheading} variant="h6" align="left">Term 7: </Typography>
+            {term7Courses().map((courseInformation) => (
+            <Container maxWidth="sm" className={classes.container}>
+              <Accordion className={classes.card}>
+                  <AccordionSummary 
+                      expandIcon={<ExpandMoreIcon />}>
+                      <div>
+                          <Typography className={classes.title} variant="body1">{courseInformation.CourseID} {courseInformation.CourseTitle}</Typography>
+                      </div>
+                  </AccordionSummary>
+                  <AccordionDetails className={classes.details}>
+                      <div>
+                          <Typography className={classes.details} variant="body4">{courseInformation.CourseDescription}</Typography>
+                      </div>
+                  </AccordionDetails>
+                  <AccordionDetails className={classes.details}>
+                      <div>
+                          <Typography className={classes.details} variant="body5">Minimum Grade: {courseInformation.MinimumGrade.toUpperCase()}</Typography>
+                      </div>
+                  </AccordionDetails>
+                  <AccordionDetails className={classes.details}>
+                      <div>
+                          <Typography className={classes.details} variant="body6">Credit Hours: {courseInformation.CreditHours}</Typography>
+                      </div>
+                  </AccordionDetails>
+                  <AccordionDetails className={classes.details}>
+                      <div>
+                          <Typography className={classes.details} variant="body7">Prerequisites: {courseInformation.PreReqs}</Typography>
+                      </div>
+                  </AccordionDetails>
+                  <AccordionDetails className={classes.details}>
+                      <div>
+                          <Typography className={classes.details} variant="body8">Corequisites: {courseInformation.CoReqs}</Typography>
+                      </div>
+                  </AccordionDetails>
+              </Accordion>
+            </Container>
+            ))}
+                        <Typography className={classes.subheading} variant="h6" align="left">Term 8: </Typography>
+            {term8Courses().map((courseInformation) => (
             <Container maxWidth="sm" className={classes.container}>
               <Accordion className={classes.card}>
                   <AccordionSummary 
